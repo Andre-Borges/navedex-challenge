0f5988f38ec15074a45028317cd4eea6c46f2dc3
André Borges <hoilut@hotmail.com>
2021-03-10 12:02:59 -0300

    function to format date

---
+++ src/utils/global.js
@@ -0,0 +1,11 @@
+const formatDateToLocaleString = (str) => {
+  if (!str) return '';
+  let data = new Date(str);
+
+  return `${('00' + data.getUTCDate()).slice(-2)}/${(
+    '00' +
+    (data.getUTCMonth() + 1)
+  ).slice(-2)}/${data.getUTCFullYear()}`;
+};
+
+export { formatDateToLocaleString };

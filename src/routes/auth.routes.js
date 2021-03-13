f3af50b991d72a757dea0299a29909087016ebd6
André Borges <hoilut@hotmail.com>
2021-03-13 16:50:07 -0300

    Fix routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
@@ -9,6 +9,8 @@ export default function AuthRoutes() {
     <BrowserRouter>
       <Route path="/" exact component={Login} />
       <Route path="/signUp" component={SignUp} />
+      {/* Evita que vai pra outra rota sem estar autenticado */}
+      <Redirect to="/" />
     </BrowserRouter>
   );
 }
